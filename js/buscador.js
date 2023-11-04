286a4ae6ec4f71f81012704c54b07c65e0e8c243
RobVallejos <vallejos.robb@gmail.com>
2023-11-04 14:09:48 -0300

    Cambios generales main page

--- js/buscador.js
+++ js/buscador.js
@@ -1,6 +1,32 @@
 $(function(){
+    let ida = $("#inputIda");
+    let idaVuelta = $("#inputIdaVuelta");
+    let divRegreso = $("#divRegreso");
     let fechaSalida = $("#fechaSalida");
     let fechaRegreso = $("#fechaRegreso");
+
+    // Seleccion tipo de viaje
+    ida.attr("checked", "checked");
+    fechaRegreso.attr("disabled", true);
+    divRegreso.css("display", "none");
+
+    $("input[type='radio']").click(function(){
+        var radioValue = $("input[name='recorrido']:checked").val();
+        if(radioValue == 1){
+            fechaRegreso.attr("disabled", true);
+            divRegreso.css("display", "none");
+        }
+        else{
+            fechaRegreso.attr("disabled", false);
+            divRegreso.css("display", "block");
+        }
+    });
+
+    let destinos = ["Retiro", "Rosario", "Mendoza", "Salta", "Villa General Belgrano", "Iguazú"];
+
+    $( "input[name='ciudad']" ).autocomplete({
+        source: destinos
+    });
 
     let fecha = new Date();
     let fechaFormateada = convertirFecha(fecha)
@@ -40,4 +66,27 @@ $(function(){
         return yyyy + "-" + mm + "-" + dd;
     }
 
+
+    let viajes = {
+        asientos : {
+            ejecutivo: {
+                asientos: 6,
+                ubicaciones: ["1V", "2P", "3V", "4V", "5P", "6V"],
+                precio: 3500,
+                premium: true
+            },
+            cama: {
+                asientos: 6,
+                ubicaciones: [1, 2, 3, 4, 5, 6],
+                precio: 3500,
+                premium: true
+            },
+            semicama: {
+                asientos: 6,
+                ubicaciones: [1, 2, 3, 4, 5, 6],
+                precio: 3500,
+                premium: true
+            }
+        }
+    }
 });
